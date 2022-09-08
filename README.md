a8dd7489d982440d3c96ea28524079d62abdecc3
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-09-08 09:51:09 -0300

    Banco de Dados configurado

--- README.md
+++ README.md
@@ -4,5 +4,12 @@ Uploader e encoder de vídeos do curso FullCycle 3.0
 
 ## Iniciando
  - [X] Criando entidades
- - [ ] Validando entidades Video e criando testes
- - [ ]  Validando Job e criando testes
+ - [X] Validando entidades Video e criando testes
+ - [X] Validando Job e criando testes
+
+## Banco de dados e Repositories
+ - [ ] Configurando o banco de dados
+ - [ ] Configurando entidades
+ - [ ] Implementando video repository
+ - [ ] Testando video repository
+ - [ ] Criando e testando jobrepository
--- domain/video.go
+++ domain/video.go
@@ -1,10 +1,32 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+)
 
 type Video struct {
-	ID         string
-	ResourceID string
-	filepath   string
-	createdAt  time.Time
+	ID         string    `valid:"uuid"`
+	ResourceID string    `valid:"notnull"`
+	Filepath   string    `valid:"notnull"`
+	CreatedAt  time.Time `valid:"-"`
+}
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+func NewVideo() *Video {
+	return &Video{}
+}
+
+func (video Video) Validate() error {
+	_, err := govalidator.ValidateStruct(video)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
